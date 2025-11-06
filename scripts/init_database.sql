edf443977ecca62e53e99a136bc29b81690d5583
mohammedneqqaz-collab <mohammed.neqqaz@usmba.ac.ma>
2025-11-06 23:16:19 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,22 @@
+USE master;
+GO
 
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'Datawarehouse')
+BEGIN
+ALTER DATABASE datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
+DROP DATABASE Datawarehouse;
+END;
+GO
+
+CREATE DATABASE Datawarehouse;
+GO
+USE Datawarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
