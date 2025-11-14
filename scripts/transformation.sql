7c593e12ef5f66e0f3a01623e7c527a6d397943c
mohammedneqqaz-collab <mohammed.neqqaz@usmba.ac.ma>
2025-11-14 17:54:21 +0100

    Create transformation.sql

---
+++ scripts/transformation.sql
@@ -0,0 +1,21 @@
+SELECT
+cst_id,
+cst_key,
+cst_firstname,
+cst_lastname,
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+     WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+     ELSE 'Not Availabale'
+END cst_marital_status,
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'female'
+     WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+     ELSE 'Not Availabale'
+END cst_gndr,
+cst_create_date
+FROM (
+SELECT 
+*,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+FROM [bronze].[cust_info] 
+WHERE cst_id IS NOT NULL
+)t WHERE flag_last = 1;
