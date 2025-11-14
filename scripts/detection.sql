f84cafa7f524fc11c5e43a379d3e6447ac25388c
mohammedneqqaz-collab <mohammed.neqqaz@usmba.ac.ma>
2025-11-14 16:04:52 +0100

    Update detection.sql

--- scripts/detection.sql
+++ scripts/detection.sql
@@ -2,3 +2,15 @@ SELECT cst_id,
 COUNT(*) FROM [bronze].[cust_info]
 GROUP BY  cst_id 
 HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+SELECT cst_firstname FROM [bronze].[cust_info]
+WHERE cst_firstname != TRIM(cst_firstname);
+
+SELECT cst_lastname FROM [bronze].[cust_info]
+WHERE cst_lastname != TRIM(cst_lastname);
+
+SELECT cst_key FROM [bronze].[cust_info]
+WHERE cst_key != TRIM(cst_key);
+
+SELECT cst_gndr FROM [bronze].[cust_info]
+WHERE cst_gndr != TRIM(cst_gndr);
